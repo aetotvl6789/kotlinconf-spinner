b7b7af51c62817e2d0dde9f01c91c360583f7ad8
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-05-21 00:13:51 +0700

    Delete getopt directory

--- getopt/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    kotlin("multiplatform")
-}
-
-kotlin {
-    val hostPresetName: String by rootProject.extra
-    targetFromPreset(presets[hostPresetName], "host")
-}
--- getopt/make_klib.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-PATH=$KONAN_HOME/bin:$PATH
-
-konanc -produce library src/hostMain/kotlin -o kliopt.klib
