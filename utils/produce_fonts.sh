f8a4b6a1511b2c46925974c1931b8f47aa822083
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-05-21 00:12:44 +0700

    Delete utils directory

--- utils/produce_fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-androidFont="$ANDROID_HOME/platforms/android-25/data/fonts/Roboto-Black.ttf"
-iosFont="/System/Library/Fonts/Menlo.ttc"
-fontSize=100
-
-./FontGenerator.kexe -f $androidFont -s $fontSize -d ./android -c $alphabet
-./FontGenerator.kexe -f $iosFont -s $fontSize -d ./ios -c $alphabet
